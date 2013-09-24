4a44b4adbf88c5b63a2d606a538ff4938a9b0c28
Jared Mashburn <jmashburn@livegamer.com>
2013-09-24 13:08:21 -0400

    Adding ElementsTest

---
+++ test/Elements/ElementsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+abstract class ElementsTest extends UnitTestCase {
+
+	public function setUp() {
+		$baseUrl = getEnv('ELEMENTS_BASEURL');
+		$partnerId = getEnv('ELEMENTS_PARTNER_ID');
+		$appFamilyId = getEnv('ELEMENTS_APP_FAMILY_ID');
+		$accessKey = getEnv('ELEMENTS_ACCESS_KEY');
+
+		if (!$baseUrl || !$partnerId || !$appFamilyId || !$accessKey) {
+			die("You must set Enviroment Variables: ELEMENTS_BASEURL, ELEMENTS_PARTNER_ID, ELEMENTS_APP_FAMILY_ID, ELEMENTS_KEY\n");
+		}
+		
+		\Elements::setBaseUrl($baseUrl);
+		\Elements::setPartnerId($partnerId);
+		\Elements::setAppFamilyId($appFamilyId);
+		\Elements::setAccessKey($accessKey);
+	}
+
+	protected static function randomString() {
+		$chars = "abcdefghijklmnopqrstuvwxyz";
+		$str = "";
+		for ($i = 0; $i < 10; $i++) {
+			$str .= $chars[rand(0, strlen($chars)-1)];
+		}
+		return $str;
+	}
+
+}
